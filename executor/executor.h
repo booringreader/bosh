79e3b29b86c09c715ca54cc57467d678928383ea
Shubham Sharma <shubhamsh.z1O1@gmail.com>
2024-01-13 19:27:53 +0530

    executor header declared

---
+++ executor/executor.h
@@ -0,0 +1,10 @@
+#ifndef BACKEND_H
+#define BACKEND_H
+
+#include "../node/node.h"
+
+char *search_path(char *file); // takes the name of command and looks for executables in directories inside $PATH variable
+int do_exec_cmd(int *argc, char **argv); //executes a command
+int do_simple_command(struct node_s *node); // main func. of executor
+
+#endif
